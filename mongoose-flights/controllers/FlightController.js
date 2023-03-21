f92c04b1be5d7df75b54eb73b26babde981628a8
Ryan Agard <ryantagard@yahoo.com>
2023-03-21 15:09:34 -0400

    added controller models etc

---
+++ mongoose-flights/controllers/FlightController.js
@@ -0,0 +1,116 @@
+const Flights = require('../models/FlightModel')
+// const Flights = require('../models/Flights')
+
+module.exports.seed = async (req, res) => {
+    await Flights.deleteMany({})
+    await Flights.create(Flights)
+    res.redirect('/flights')
+}
+
+module.exports.index = async (req, res) => {
+    const Flights = await Flights.find().sort({ createdAt: 1 })
+    res.render('flights/Index', { Flights })
+}
+
+module.exports.new = async (req, res) => {
+    res.render('flights/New')
+}
+
+module.exports.delete = async (req, res) => {
+    await Flights.findByIdAndDelete(req.params.id)
+    res.redirect('/flights')
+}
+
+module.exports.update = async (req, res) => {
+    await Flights.findByIdAndUpdate(req.params.id, req.body)
+    res.redirect(`/flights/${req.params.id}`)
+}
+
+module.exports.create = async (req, res) => {
+    console.log(req.body)
+    try {
+        await Flights.create(req.body)
+        res.redirect('/flights')
+    } catch(err) {
+        res.send(err.message)
+    }
+}
+
+module.exports.edit = async (req, res) => {
+    const Flight = await Flights.findById(req.params.id)
+    console.log(Flight)
+    console.log('edit route')
+    res.render('flights/Edit', { Flight })
+}
+
+module.exports.show = async (req, res) => {
+    const Flight = await Flights.findById(req.params.id)
+    console.log(Flight)
+    res.render('flights/Show', { Flight })
+}
+
+
+// EXTRA LOGIC (for comments)
+
+module.exports.createComment = async (req, res) => {
+    // create a comment by updating the comments property in Flight
+    await Flights.findByIdAndUpdate(req.params.id, {
+        // push the req.body to the comments property/field of this Flight document
+        $push: {
+            comments: req.body
+        }
+    })
+    res.redirect(`/Flights/${req.params.id}`)
+}
+
+module.exports.deleteComment = async (req, res) => {
+    // delete a comment by updating the comments property in Flight
+    await Flights.findByIdAndUpdate(req.params.id, {
+        // pulling out or removing a subdocument  
+        $pull: {
+            // from the comments property/field
+            comments: {
+                // with a matching comment id
+                _id: req.params.cid
+            }
+        }
+    })
+    res.redirect(`/Flights/${req.params.id}`)
+}
+
+module.exports.indexComment = async (req, res) => {
+    // target the comments property 
+    const Flight = await Flights.findById(req.params.id)
+    res.send(Flight.comments)
+}
+
+module.exports.showComment = async (req, res) => {
+    // find the Flight and filter it's comments property array
+    const Flight = await Flights.findById(req.params.id)
+    const [ comment ] = Flight.comments.filter(comment => comment._id.toString() === req.params.cid) 
+    res.render('comments/Edit', { FlightId: req.params.id, comment })
+}
+
+module.exports.updateComment = async (req, res) => {
+    // update a comment by updating an item in the comments property in Flight
+
+    // OPTION 1: using only Mongo operators and queries (more confusing)
+console.log(req.body) 
+    // find the Flight with the matching id, then check that Flight's comments for matching comment id
+    await Flights.updateOne({ _id: req.params.id, 'comments._id': req.params.cid }, {
+        // set/replace the content 
+        $set: {
+            // of the comment at index (represented by $) and change its body property   -->    comments[1].body = 'value
+            'comments.$.body': req.body.body // req.body is the form data and req.body.body is the updated value of the comment
+        }
+    })
+
+    // OPTION 2: using plain JavaScript together with Mongo queries (less efficient)
+
+    // const Flight = await Flights.findById(req.params.id)
+    // const index = Flight.comments.findIndex(comment => comment._id.toString() === req.params.cid)
+    // Flight.comments[index].body = req.body.body
+    // await Flight.save()
+
+    res.redirect(`/Flights/${req.params.id}`)
+}
